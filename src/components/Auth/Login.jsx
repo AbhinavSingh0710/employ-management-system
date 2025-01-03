99d27384131026fd8ac3f095eafa1be12a70137e
AbhinavSingh0710 <abhinav.singh0718@gmail.com>
2025-01-03 15:13:26 +0530

    first commit

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Email is", email);
+    console.log("Password is", password);
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="flex h-screen w-screen items-center justify-center ">
+      <div className="border-2 border-emerald-600 p-20 rounded-xl">
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="flex flex-col items-center justify-center"
+        >
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+            className="mb-3 outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 px-5 text-xl placeholder:text-gray-400"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+            className="outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 px-5 text-xl placeholder:text-gray-400"
+            type="password"
+            placeholder="Enter password"
+          />
+          <button className="mt-5 w-full text-white outline-none border-none bg-emerald-600 rounded-full py-2 px-3 text-xl">
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div
+      id="tasklist"
+      className="h-[57%] flex items-center justify-start gap-5 w-full py-5 mt-10 flex-nowrap overflow-x-auto"
+    >
+      <div className="flex-shrink-0 h-full w-[300px] bg-red-400 p-5 rounded-xl">
+        <div className="flex items-center justify-between">
+          <h3 className="bg-red-600 w-fit px-3 py-1 rounded text-sm">High</h3>
+          <h4 className="font-semibold text-sm mr-5 ">2 Jan 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Learn Backend</h2>
+        <p className="text-sm mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quibusdam qui ab quisquam, voluptatem inventore?</p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] bg-green-400 p-5 rounded-xl">
+        <div className="flex items-center justify-between">
+          <h3 className="bg-red-600 w-fit px-3 py-1 rounded text-sm">High</h3>
+          <h4 className="font-semibold text-sm mr-5 ">2 Jan 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Learn Backend</h2>
+        <p className="text-sm mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quibusdam qui ab quisquam, voluptatem inventore?</p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] bg-blue-400 p-5 rounded-xl">
+        <div className="flex items-center justify-between">
+          <h3 className="bg-red-600 w-fit px-3 py-1 rounded text-sm">High</h3>
+          <h4 className="font-semibold text-sm mr-5 ">2 Jan 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Learn Backend</h2>
+        <p className="text-sm mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quibusdam qui ab quisquam, voluptatem inventore?</p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 p-5 rounded-xl">
+        <div className="flex items-center justify-between">
+          <h3 className="bg-red-600 w-fit px-3 py-1 rounded text-sm">High</h3>
+          <h4 className="font-semibold text-sm mr-5 ">2 Jan 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Learn Backend</h2>
+        <p className="text-sm mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quibusdam qui ab quisquam, voluptatem inventore?</p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
